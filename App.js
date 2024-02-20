182d5da33e85abb1a50e5feac88901c3d277baf8
HighRollerLV <ip19.r.dambitis@vtdt.edu.lv>
2024-02-21 00:13:01 +0200

    Styles and Optimization

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from 'react';
-import {Text, StyleSheet, Dimensions, View, TouchableOpacity} from 'react-native';
+import React, {useCallback, useEffect, useState} from 'react';
+import {Text, Image, StyleSheet, Dimensions, View, TouchableOpacity} from 'react-native';
 import {GameEngine} from 'react-native-game-engine';
 import Coin from './src/components/Coin';
 import Block from './src/components/Block';
@@ -12,120 +12,6 @@ const defaultNumberOfCoins = 6;
 const coinWidth = 60;
 const coinHeight = 60;
 const minimumDistance = 60;
-
-export default function App() {
-    const [score, setScore] = useState(0);
-    const [lives, setLives] = useState(3);
-    const [gameStarted, setGameStarted] = useState(false);
-    const [entities, setEntities] = useState({});
-    const [resetSignal, setResetSignal] = useState(false);
-
-    useEffect(() => {
-        setEntities(generateInitialEntities(calculateActiveCoins(score))); // Adjust entities based on score
-    }, [score, gameStarted]);
-
-    function isTooClose(x, y, previousPositions) {
-        return previousPositions.some(pos =>
-            Math.sqrt(Math.pow(pos.x - x, 2) + Math.pow(pos.y - y, 2)) < minimumDistance
-        );
-    }
-
-    function generateRandomPosition(previousPositions) {
-        let x, y;
-        do {
-            x = Math.random() * (width - coinWidth); // Adjust to ensure coins start fully on-screen
-            y = -Math.random() * 500; // Ensure coins start off-screen
-        } while (isTooClose(x, y, previousPositions));
-        return {x, y};
-    }
-
-    function calculateActiveCoins(score) {
-        // Start with 5 coins, increase by 5 for every 20 points scored
-        return defaultNumberOfCoins + Math.floor(score / 20) * 5;
-    }
-
-
-    function generateInitialEntities(numCoins) {
-        let entities = {
-            block: {
-                x: width / 2 - 30,
-                y: height - 80,
-                width: 70,
-                height: 60,
-                renderer: <Block/>,
-            },
-        };
-        let previousPositions = [];
-        for (let i = 0; i < numCoins; i++) {
-            const {x, y} = generateRandomPosition(previousPositions);
-            entities[`coin_${i}`] = {
-                x,
-                y,
-                width: coinWidth,
-                height: coinHeight,
-                renderer: <Coin/>,
-            };
-            previousPositions.push({x, y});
-        }
-        return entities;
-    }
-
-    const startGame = () => {
-        setGameStarted(true);
-        setScore(0); // Reset score when game starts
-        setLives(3); // Reset lives
-        setEntities(generateInitialEntities(calculateActiveCoins(score))); // Initialize entities with dynamic number of coins based on score
-    };
-
-    const resetGame = () => {
-        setScore(0);
-        setLives(3);
-        setGameStarted(false);
-        setResetSignal(true); // Trigger a reset
-        setTimeout(() => {
-            setResetSignal(false);
-            setEntities(generateInitialEntities(calculateActiveCoins(score))); // Reset entities upon game reset with dynamic number of coins
-        }, 100);
-    };
-
-    // Conditional rendering based on game state
-    if (!gameStarted) {
-        return (
-            <View style={styles.container}>
-                <TouchableOpacity onPress={startGame} style={styles.startButton}>
-                    <Text style={styles.startButtonText}>Start Game</Text>
-                </TouchableOpacity>
-            </View>
-        );
-    }
-
-    if (lives <= 0) {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.gameOverText}>Game Over</Text>
-                <Text style={styles.finalScoreText}>Final Score: {score}</Text>
-                <TouchableOpacity onPress={resetGame} style={styles.restartButton}>
-                    <Text style={styles.restartButtonText}>Restart</Text>
-                </TouchableOpacity>
-            </View>
-        );
-    }
-
-    // Main game rendering
-    return (
-        <GameEngine
-            style={styles.gameContainer}
-            systems={[
-                MoveCoin(width, height, setLives, resetSignal, coinWidth), // Now includes coinWidth
-                CollisionDetection(setScore, width),
-                DragHandler,
-            ]}
-            entities={entities}>
-            <Text style={styles.scoreText}>Score: {score}</Text>
-            <Text style={styles.livesText}>Lives: {lives}</Text>
-        </GameEngine>
-    );
-}
 
 const styles = StyleSheet.create({
     container: {
@@ -163,26 +49,166 @@ const styles = StyleSheet.create({
         marginTop: 20,
     },
     startButton: {
-        paddingHorizontal: 20,
-        paddingVertical: 10,
-        backgroundColor: 'green',
+        paddingHorizontal: 30,
+        paddingVertical: 20,
+        backgroundColor: '#F5842A',
         borderRadius: 5,
+        borderWidth: 2,
+        borderColor: 'black',
     },
     startButtonText: {
         color: 'white',
-        fontSize: 20,
+        fontSize: 25,
         fontWeight: 'bold',
+    },
+    overlayContainer: {
+        flex: 1, // Take full height of the parent container
+        justifyContent: 'center', // Align children (the button) vertically in the center
+        alignItems: 'center', // Align children (the button) horizontally in the center
+        position: 'relative', // Allows absolute positioning of the logo relative to this container
+    },
+
+    overlayImage: {
+        position: 'absolute',
+        width: 260, // Adjust the width as needed
+        height: 260, // Adjust the height as needed
+        resizeMode: 'cover', // Keeps the aspect ratio, adjust as needed
+        top: 0, // Position at the top of the overlayContainer
+        transform: [{translateY: 100}], // Move up by half the height of the image or adjust as needed
     },
     restartButton: {
         marginTop: 20,
-        backgroundColor: '#007bff',
-        paddingHorizontal: 20,
-        paddingVertical: 10,
+        backgroundColor: '#F5842A',
+        paddingHorizontal: 30,
+        paddingVertical: 20,
         borderRadius: 5,
+        borderWidth: 2,
+        borderColor: 'black',
     },
     restartButtonText: {
         color: 'white',
-        fontSize: 20,
+        fontSize: 25,
         fontWeight: 'bold',
     },
 });
+
+export default function App() {
+    const [score, setScore] = useState(0);
+    const [lives, setLives] = useState(3);
+    const [gameStarted, setGameStarted] = useState(false);
+    const [entities, setEntities] = useState({});
+    const [resetSignal, setResetSignal] = useState(false);
+
+    useEffect(() => {
+        setEntities(generateInitialEntities(calculateActiveCoins(score))); // Adjust entities based on score
+    }, [score, gameStarted]);
+
+    const isTooClose = useCallback((x, y, previousPositions) => {
+        return previousPositions.some(pos =>
+            Math.sqrt(Math.pow(pos.x - x, 2) + Math.pow(pos.y - y, 2)) < minimumDistance
+        );
+    }, []);
+
+    const generateRandomPosition = useCallback((previousPositions) => {
+        let x, y;
+        do {
+            x = Math.random() * (width - coinWidth); // Adjust to ensure coins start fully on-screen
+            y = -Math.random() * 500; // Ensure coins start off-screen
+        } while (isTooClose(x, y, previousPositions));
+        return {x, y};
+    }, [isTooClose]);
+
+    const calculateActiveCoins = useCallback((score) => {
+        // Start with 5 coins, increase by 5 for every 20 points scored
+        return defaultNumberOfCoins + Math.floor(score / 20) * 5;
+    }, []);
+
+
+    const generateInitialEntities = useCallback((numCoins) => {
+        let entities = {
+            block: {
+                x: width / 2 - 30,
+                y: height - 80,
+                width: 70,
+                height: 60,
+                renderer: <Block/>,
+            },
+        };
+        let previousPositions = [];
+        for (let i = 0; i < numCoins; i++) {
+            const {x, y} = generateRandomPosition(previousPositions);
+            entities[`coin_${i}`] = {
+                x,
+                y,
+                width: coinWidth,
+                height: coinHeight,
+                renderer: <Coin/>,
+            };
+            previousPositions.push({x, y});
+        }
+        return entities;
+    }, []);
+
+    const startGame = useCallback(() => {
+        setGameStarted(true);
+        setScore(0); // Reset score when game starts
+        setLives(3); // Reset lives
+        setEntities(generateInitialEntities(calculateActiveCoins(score))); // Initialize entities with dynamic number of coins based on score
+    }, []);
+
+    const resetGame = useCallback(() => {
+        setScore(0);
+        setLives(3);
+        setGameStarted(false);
+        setResetSignal(true); // Trigger a reset
+        setTimeout(() => {
+            setResetSignal(false);
+            setEntities(generateInitialEntities(calculateActiveCoins(score))); // Reset entities upon game reset with dynamic number of coins
+        }, 100);
+    }, []);
+
+    // Conditional rendering based on game state
+    if (!gameStarted) {
+        return (
+            <View style={styles.container}>
+                <View style={styles.overlayContainer}>
+                    <Image source={require('./img/tolmetsLogo.png')} style={styles.overlayImage}/>
+                    <TouchableOpacity onPress={startGame} style={styles.startButton}>
+                        <Text style={styles.startButtonText}>Start Game</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    }
+
+
+    if (lives <= 0) {
+        return (
+            <View style={styles.container}>
+                <View style={styles.overlayContainer}>
+                    <Image source={require('./img/tolmetsLogo.png')} style={styles.overlayImage}/>
+                    <Text style={styles.gameOverText}>Game Over</Text>
+                    <Text style={styles.finalScoreText}>Final Score: {score}</Text>
+                    <TouchableOpacity onPress={resetGame} style={styles.restartButton}>
+                        <Text style={styles.restartButtonText}>Restart</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    }
+
+    // Main game rendering
+    return (
+        <GameEngine
+            style={styles.gameContainer}
+            systems={[
+                MoveCoin(width, height, setLives, resetSignal, coinWidth), // Now includes coinWidth
+                CollisionDetection(setScore, width),
+                DragHandler,
+            ]}
+            entities={entities}>
+            <Text style={styles.scoreText}>Score: {score}</Text>
+            <Text style={styles.livesText}>Lives: {lives}</Text>
+        </GameEngine>
+    );
+}
--- src/components/Coin.js
+++ src/components/Coin.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import { Image, StyleSheet } from 'react-native';
 
-const Coin = ({ x, y, width, height }) => {
+const Coin = React.memo(({ x, y, width, height }) => {
     return (
         <Image
-            source={require('../../img/voyagerCar1.png')} // Ensure the path is correct
+            source={require('../../img/voyagerCar1.png')}
             style={[styles.image, { left: x, top: y, width: width, height: height }]}
-            resizeMode="cover" // Adjust as needed to fit your design
+            resizeMode="cover"
         />
     );
-};
+}, (prevProps, nextProps) => {
+    return prevProps.x === nextProps.x && prevProps.y === nextProps.y && prevProps.width === nextProps.width && prevProps.height === nextProps.height;
+});
 
 const styles = StyleSheet.create({
     image: {
-        position: 'absolute', // Keeps the absolute positioning for layout
+        position: 'absolute',
     },
 });
 
 export default Coin;
+
 
 
 
